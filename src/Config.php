c470a1e3e7f16e75db325e671491d4443361e39d
AlexdeCampos <alexcampos.dev@gmail.com>
2023-11-26 22:48:56 -0300

    Estrutura básica do projeto em MVC com docker , mysql, php e nginx

---
+++ src/Config.php
@@ -0,0 +1,13 @@
+<?php
+namespace src;
+
+class Config {
+    const BASE_DIR = 'localhost:8080';
+
+    const DB_DRIVER = 'mysql';
+    const DB_HOST = 'bis2bisdb';
+    const DB_DATABASE = 'bis2bisdb';
+    CONST DB_USER = 'root';
+    const DB_PASS = 'admin';
+
+}
